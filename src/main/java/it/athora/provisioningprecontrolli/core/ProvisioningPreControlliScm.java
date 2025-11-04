7130600cec20d111e04bf67e165f90d2896ae999
nutbuster90001 <nutbuster90001@gmail.com>
2025-11-04 14:40:50 +0100

    Add Logback configuration and migrate logging to SLF4J

--- src/main/java/it/athora/provisioningprecontrolli/core/ProvisioningPreControlliScm.java
+++ src/main/java/it/athora/provisioningprecontrolli/core/ProvisioningPreControlliScm.java
@@ -1,19 +1,19 @@
 package it.athora.provisioningprecontrolli.core;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import it.athora.provisioningprecontrolli.bo.AgenziaListino;
 import it.athora.provisioningprecontrolli.business.ProvisioningPreControlliBusiness;
 
 public class ProvisioningPreControlliScm {
-	private static final Logger logger = Logger.getLogger(ProvisioningPreControlliScm.class.getName());
+        private static final Logger logger = LoggerFactory.getLogger(ProvisioningPreControlliScm.class);
 
 	public static void main(String[] args) {
 		try {
 			new ProvisioningPreControlliBusiness(AgenziaListino.SCM).execute();
 		} catch (Throwable e) {
-			logger.log(Level.SEVERE, "Errore in procedura: ", e);
+                        logger.error("Errore in procedura", e);
 			System.out.println("Errore in procedura");
 		}
 	}
